d975ff5d6d440d817966c9b6d095f4fc62c2a0ab
Mamatha <Mamatha@LAPTOP-7J9ULA4Q>
2022-12-01 19:24:42 +0530

    hello

---
+++ src/main/java/acoecseb/Mamatha5a8/App.java
@@ -0,0 +1,13 @@
+package acoecseb.Mamatha5a8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
